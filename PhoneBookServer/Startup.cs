0eaee4084069ab3b1881a46610d831f07229ad93
Miloš Spasojević <mspasojevic@showingtime.com>
2018-03-23 14:52:48 +0100

    Basic client implementation. Enabled CORS on server side

--- PhoneBookServer/Startup.cs
+++ PhoneBookServer/Startup.cs
@@ -1,13 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
 
 namespace PhoneBookServer
 {
@@ -23,6 +17,7 @@ namespace PhoneBookServer
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors();
             services.AddMvc();
         }
 
@@ -33,6 +28,9 @@ namespace PhoneBookServer
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseCors(options=> options.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod());
+
 
             app.UseMvc();
         }
--- phone-book-client/src/App.js
+++ phone-book-client/src/App.js
@@ -1,22 +1,57 @@
 import React, { Component } from 'react';
 import './App.css';
+import axios from "axios";
 
 class App extends Component {
   constructor(props) {
     super(props);
-    // let phoneNumber = { number: "+381 60 412 84 22", "numberType": 0 }
-    // let phoneNumber2 = { number: "+381 69 212 84 22", "numberType": 1 }
-    // let contact = { firstName: "Miloš", lastName: "Spasojević", phoneNumbers: [phoneNumber, phoneNumber2] }
-    this.state = {contacts: []};
+    this.state = { contacts: [], newContactFirstName: "", newContactLastName: "", newContactPhoneNumber: "" };
   }
-  
-  componentDidMount() {
+
+
+  loadAllContacts() {
     var _this = this;
-    // this.serverRequest = axios.get("http://codepen.io/jobs.json").then(function(result) {    
-    //       _this.setState({
-    //         contacts: result.data.jobs
-    //       });
-    //     })
+    this.serverRequest = axios.get("http://phonebookserver20180323113131.azurewebsites.net/api/contacts").then(function (result) {
+      _this.setState({
+        contacts: result.data
+      });
+    }).catch(error => {
+      console.log(error);
+    });
+  }
+
+  handleNameChange(event) {
+    this.setState({ newContactFirstName: event.target.value });
+  }
+
+  handleLastNameChange(event) {
+    this.setState({ newContactLastName: event.target.value });
+  }
+
+  handlePhoneNumberChange(event) {
+    this.setState({ newContactPhoneNumber: event.target.value });
+  }
+
+  addNewNumber() {
+    var number = { numberType: 0, number: this.state.newContactPhoneNumber };
+    var contact = { firstName: this.state.newContactFirstName, lastName: this.state.newContactLastName, phoneNumbers: [number] };
+    var _this = this;
+    axios({
+      method: 'post',
+      url: 'http://phonebookserver20180323113131.azurewebsites.net/api/contacts',
+      headers: { 'Access-Control-Allow-Origin': '*' },
+      data: contact
+    }).then(function (result) {
+      this.setState({ newContactFirstName: "", newContactLastName: "", newContactPhoneNumber: "" });
+      _this.loadAllContacts();
+    }).catch(error => {
+      alert('there was a problem creating the contact');
+      console.log(error);
+    });
+  }
+
+  componentDidMount() {
+    this.loadAllContacts();
   }
 
   render() {
@@ -26,31 +61,76 @@ class App extends Component {
           <h1 className="App-title">Welcome to the PhoneBook application!</h1>
         </header>
         <div className="App-intro">
-          {this.state.contacts.map(c=> { return <ContactComponent contact={c} />})}
-          
+          <p>
+            Your contacts:
+          </p>
+          <button onClick={() => { this.loadAllContacts() }}>Refresh</button>
+          <table>
+            <thead>
+              <tr>
+                <th>First Name</th>
+                <th>Last Name</th>
+                <th>PhoneNumbers</th>
+              </tr>
+            </thead>
+
+            <tbody>
+              {this.state.contacts.map(c => {
+                return (
+                  <tr key={c.id}>
+                    <ContactComponent contact={c} />
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+          <form>
+            <label>
+              First name:
+            <input type="text" value={this.state.newContactFirstName} onChange={(e) => this.handleNameChange(e)} />
+            </label>
+            <br />
+            <label>
+              Last name:
+            <input type="text" value={this.state.newContactLastName} onChange={(e) => this.handleLastNameChange(e)} />
+            </label>
+            <br />
+            <label>
+              Phone number:
+            <input type="text" value={this.state.newContactPhoneNumber} onChange={(e) => this.handlePhoneNumberChange(e)} />
+            </label>
+            <br />
+
+          </form>
+          <button onClick={() => this.addNewNumber()}>Add new</button>
         </div>
       </div>
     );
   }
 }
 
-class PhoneNumberComponent extends Component {
+class PhoneNumbersComponent extends Component {
   render() {
     return (
-      <div>{this.props.phoneNumber.number}</div>
+      this.props.numbers.map(n => {
+        return (
+          <td key={n.number}>{n.number}</td>
+        );
+      })
     );
   }
 }
+
 class ContactComponent extends Component {
   render() {
-    return (
-      <div>
-        <div>{this.props.contact.firstName} {this.props.contact.lastName}</div>
-        <div>{this.props.contact.phoneNumbers.map(pn => {
-          return <PhoneNumberComponent phoneNumber={pn} />
-        })}</div>
-      </div>
-    );
+    var cells = [];
+    cells.push(<td key={1}>{this.props.contact.firstName}</td>);
+    cells.push(<td key={2}>{this.props.contact.lastName}</td>);
+    if (this.props.contact.phoneNumbers !== undefined && this.props.contact.phoneNumbers.length > 0) {
+      cells.push(<PhoneNumbersComponent key={3} numbers={this.props.contact.phoneNumbers} />);
+    }
+
+    return cells;
   }
 }
 
