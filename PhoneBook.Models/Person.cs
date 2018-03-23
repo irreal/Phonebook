459593eb4eba62d2a9877a28492b04eae4e33766
Miloš Spasojević <mspasojevic@showingtime.com>
2018-03-23 11:44:43 +0100

    Added Person model, Referenced models from Server

---
+++ PhoneBook.Models/Person.cs
@@ -0,0 +1,16 @@
+﻿namespace PhoneBook.Models
+{
+    public class Person
+    {
+        public Person(string firstName, string lastName = null)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+        }
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public string FullName => $"{FirstName} {LastName ?? string.Empty}".Trim(); // .Trim() removes empty trailing space if there is no last name defined
+    }
+}
